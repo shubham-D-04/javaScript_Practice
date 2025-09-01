d250008c8444e44daf82876436737ef22970f5ab
Shubham Dafare <shubhamdafare4@gmail.com>
2025-09-01 13:36:54 +0530

    Hello World Practice Code Solution.

---
+++ hello_World.js
@@ -0,0 +1,6 @@
+/*-----------------------------------------------
+Write a Program to Print "Hello, World!" in JavaScript.
+--------------------------------------------------*/
+
+console.log("Hello, World!");
+console.log("I have Started Learning JavaScript!!");
