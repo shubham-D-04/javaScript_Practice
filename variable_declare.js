040e6c71b57f913f237c1188ca4a8a59d3ca6a7b
Shubham Dafare <shubhamdafare4@gmail.com>
2025-09-01 13:53:36 +0530

    Variable Declare Solution Code.

---
+++ variable_declare.js
@@ -0,0 +1,11 @@
+/* ------------------------------------------------ 
+Write a program to declare a variable and print it.
+--------------------------------------------------*/
+
+let num = 10;
+const str_name = "Michael";
+let float_num = 10.5;
+
+console.log("Number Variable:" + num);
+console.log("String:" + str_name);
+console.log("Float or Fraction Number:" + intnum);
