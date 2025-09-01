bf7e362838de4c9482c351c2f4f94b01eca818ae
Shubham Dafare <shubhamdafare4@gmail.com>
2025-09-01 14:23:05 +0530

    hello_World add new console.log Statement.

--- hello_World.js
+++ hello_World.js
@@ -4,3 +4,6 @@ Write a Program to Print "Hello, World!" in JavaScript.
 
 console.log("Hello, World!");
 console.log("I have Started Learning JavaScript!!");
+console.log(
+  "Most people quit too early, but failing is how you learn, and it's important to give it time and respect the process"
+);
